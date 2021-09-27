376604ebf4a3dcc8e205f975af553dbb5a514760
Rasitha Chathuranga <90918661+RasithaCLiyanage@users.noreply.github.com>
2021-09-27 18:42:55 +0530

    Create queries to Revisit.sql

---
+++ queries to Revisit.sql
@@ -0,0 +1,9 @@
+-- getting tables information in T-sql
+
+select schema_name(t.schema_id) as schema_name,
+       t.name as table_name,
+       t.create_date,
+       t.modify_date
+from sys.tables t
+order by schema_name,
+         table_name;
