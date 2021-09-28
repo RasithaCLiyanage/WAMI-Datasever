5a90cfb473c9b01a0e4c9699d78e847bb210f351
Rasitha Chathuranga <90918661+RasithaCLiyanage@users.noreply.github.com>
2021-09-28 20:20:44 +0530

    Update queries to Revisit.sql

--- queries to Revisit.sql
+++ queries to Revisit.sql
@@ -11,3 +11,16 @@ order by schema_name,
          
 -- this is stored procedure query to rename table name in T-sql
 EXEC sp_rename '[dbo].[order_line_Legacy]', 'order_line_96586'
+
+-- this is used by Ashen to see the number of rows available in the table orderline
+begin
+    declare @from datetime2;
+    set @from = dateadd(day, -120, cast(concat(cast(getdate() as date), ' 00:00:00.000000') as datetime2));
+    declare @to datetime2;
+    set @to = dateadd(day, -1, cast(concat(cast(getdate() as date), ' 00:00:00.000000') as datetime2));
+    select count(*)
+    from [dbo].[order_line_96586]
+    where [dbo].[order_line_96586].timestamp > @from
+      and [dbo].[order_line_96586].timestamp <= @to;
+end
+go;
